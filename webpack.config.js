636b4b3aebe31b5b2df24641f52d64090380a713
Corey Larson <coreyleelarson@gmail.com>
2019-01-24 17:08:28 -0600

    Updates webpack configuration and module dists.

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,11 @@ const babelConfig = require('./babel.config')
 module.exports = {
   mode: 'production',
   output: {
-    filename: 'index.js'
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
   },
   externals: {
-    react: {
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'React',
-      root: 'React'
-    },
-    'react-dom': {
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'ReactDOM',
-      root: 'ReactDOM'
-    }
+    react: 'commonjs react'
   },
   module: {
     rules: [
